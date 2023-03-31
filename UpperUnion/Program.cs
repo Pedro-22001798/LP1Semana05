e8fac9c373612efec9d3c686307e0b77c8a2fec9
Pedro Silva <73141072+Pedro-22001798@users.noreply.github.com>
2023-03-31 11:35:44 +0100

    UpperUnion Done

--- UpperUnion/Program.cs
+++ UpperUnion/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+
+namespace UpperUnion
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string[] newArray = Array.ConvertAll(args,s => s.ToUpper());
+            Array.Sort(newArray);
+
+            string joinString = String.Join("-",newArray);
+
+            if(args.Length != 0)
+            {
+                Console.WriteLine(joinString);
+            }
+            else if(args.Length == 0)
+            {
+                Console.WriteLine("NO ARGUMENTS");
+            }
+        }
+    }
+}

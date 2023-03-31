925f543dfad718f280a43739497b13b1418800fc
Pedro Silva <73141072+Pedro-22001798@users.noreply.github.com>
2023-03-31 10:54:08 +0100

    First try on dices

--- RandomDice/Program.cs
+++ RandomDice/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace RandomDice
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Olá! Quantos dados quer lançar?");
+            int numDices = Convert.ToInt32(Console.ReadLine());
+
+            Random dice = new Random();
+            int sumDices = 0;
+
+            for(int i = 0; i < numDices; i++)
+            {
+                sumDices += dice.Next(1,7);
+            }
+
+            Console.WriteLine($"A soma dos dados é {sumDices}");
+        }
+    }
+}

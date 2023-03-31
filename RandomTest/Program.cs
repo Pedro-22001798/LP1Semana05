bda8375e886077168d33c5c9d800486c3cda4cca
Pedro Silva <73141072+Pedro-22001798@users.noreply.github.com>
2023-03-31 10:45:00 +0100

    Tested every option for Random seed

--- RandomTest/Program.cs
+++ RandomTest/Program.cs
@@ -8,11 +8,16 @@ namespace RandomTest
         {
             Random r1 = new Random(22);
             Random r2 = new Random(22);
+            r2 = new Random(444);
             for (int i = 0; i < 4; i++)
                 Console.WriteLine(r1.Next());
             Console.WriteLine();
             for (int i = 0; i < 4; i++)
                 Console.WriteLine(r2.Next());
+            Console.WriteLine();
+            Random r3 = new Random(323);
+            for (int i = 0; i < 4; i++)
+                Console.WriteLine(r3.Next());            
         }
     }
 }
